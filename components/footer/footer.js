80fc2ce25fbe4d09f2d9b6235ff8b03716710217
Attila Gyori <attila.gyori.id@gmail.com>
2021-11-18 22:57:02 +0100

    footer with content

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,13 +1,23 @@
 /** @format */
 
 import Logo from "../logo/logo";
+import Social from "../social/social";
 import classes from "./footer.module.scss";
 export default function Footer() {
   return (
     <div className={classes["footer-container"]}>
-      <Logo />
-      <div>soveg</div>
-      <div>soveg</div>
+      <div className={classes["logo"]}>
+        <Logo />
+      </div>
+
+      <div className={classes["text"]}>
+        <div>MANUFACTURED</div>
+        <div className={classes["by"]}>by attila győri</div>
+        <div className={classes["date"]}>&copy;2021</div>
+      </div>
+      <div className={classes["social-block"]}>
+        <Social layout='landscape' />
+      </div>
     </div>
   );
 }
