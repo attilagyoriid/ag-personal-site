5ac192651500a709d5867ea299f0afa28c490ba9
Attila Gyori <attila.gyori.id@gmail.com>
2021-11-21 14:13:11 +0100

    Correct video path

--- components/videoIntro/videoIntro.jsx
+++ components/videoIntro/videoIntro.jsx
@@ -6,8 +6,8 @@ import bkgImg from "./bkg.png";
 export default function VideoIntro() {
   return (
     <div className={classes["video-container"]}>
-      <video autoPlay muted poster={bkgImg}>
-        <source src='/video/_intro.mp4' type='video/mp4'></source>
+      <video autoPlay muted poster='/video/bkg.png'>
+        <source src='/video/intro.mp4' type='video/mp4'></source>
       </video>
     </div>
   );
