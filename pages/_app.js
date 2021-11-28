8ec65b351fc43c9dbf156842995a580dd14bfeb8
Attila Gyori <attila.gyori.id@gmail.com>
2021-11-28 09:11:57 +0100

    framer-motion page transition

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,40 @@
 /** @format */
 
 import "../styles/globals.scss";
+import { motion } from "framer-motion";
+import nProgress from "nprogress";
+import { Router } from "next/router";
+import "nprogress/nprogress.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
+
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <motion.div
+      key={router.route}
+      initial='initial'
+      animate='animate'
+      variants={{
+        initial: {
+          opacity: 0,
+          scale: 1,
+        },
+        animate: {
+          opacity: 1,
+          scale: 1,
+        },
+        exit: {
+          opacity: 0,
+          scale: 0.4,
+        },
+      }}
+      transition={{ duration: 0.9 }}
+    >
+      <Component {...pageProps} key={router.route} />
+    </motion.div>
+  );
 }
 
 export default MyApp;
