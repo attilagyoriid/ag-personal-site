d254fe1a788839ace3865e68c0a630d61b9ccdf1
Attila Gyori <attila.gyori.id@gmail.com>
2021-12-17 11:32:51 +0100

    social links

--- components/social/social.jsx
+++ components/social/social.jsx
@@ -1,7 +1,12 @@
 /** @format */
 import classes from "./social.module.scss";
+import getConfig from "next/config";
 
 export default function Social({ layout }) {
+  const {
+    publicRuntimeConfig: { socialUrls },
+  } = getConfig();
+
   const orientation =
     layout === "landscape"
       ? "social-icon-container-horizontal"
@@ -9,22 +14,38 @@ export default function Social({ layout }) {
   return (
     <div className={classes[orientation]}>
       <div className={classes["icon-container"]}>
-        <img src='/images/social-icons/linked.svg' alt='Linked In Icon' />
+        <a target='_blank' href={socialUrls.linkedin} rel='noopener noreferrer'>
+          <img src='/images/social-icons/linked.svg' alt='Linked In Icon' />
+        </a>
       </div>
       <div className={classes["icon-container"]}>
-        <img src='/images/social-icons/git.svg' alt='Linked In Icon' />
+        <a target='_blank' href={socialUrls.github} rel='noopener noreferrer'>
+          <img src='/images/social-icons/git.svg' alt='Linked In Icon' />
+        </a>
       </div>
       <div className={classes["icon-container"]}>
-        <img src='/images/social-icons/yout.svg' alt='Linked In Icon' />
+        <a target='_blank' href={socialUrls.youtube} rel='noopener noreferrer'>
+          <img src='/images/social-icons/yout.svg' alt='Linked In Icon' />
+        </a>
       </div>
       <div className={classes["icon-container"]}>
-        <img src='/images/social-icons/dribble.svg' alt='Linked In Icon' />
+        <a target='_blank' href={socialUrls.dribbble} rel='noopener noreferrer'>
+          <img src='/images/social-icons/dribble.svg' alt='Linked In Icon' />
+        </a>
       </div>
       <div className={classes["icon-container"]}>
-        <img src='/images/social-icons/face.svg' alt='Linked In Icon' />
+        <a target='_blank' href={socialUrls.facebook} rel='noopener noreferrer'>
+          <img src='/images/social-icons/face.svg' alt='Linked In Icon' />
+        </a>
       </div>
       <div className={classes["icon-container"]}>
-        <img src='/images/social-icons/insta.svg' alt='Linked In Icon' />
+        <a
+          target='_blank'
+          href={socialUrls.instagram}
+          rel='noopener noreferrer'
+        >
+          <img src='/images/social-icons/insta.svg' alt='Linked In Icon' />
+        </a>
       </div>
     </div>
   );
--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
-}
+  publicRuntimeConfig: {
+    socialUrls: {
+      facebook: "https://www.facebook.com/gyoriattila",
+      linkedin: "https://www.linkedin.com/in/attila-gyori-ag",
+      github: "https://github.com/attilagyoriid",
+      instagram: "https://www.instagram.com/attila_gyori",
+      dribbble: "https://dribbble.com/attilagyori",
+      youtube: "https://www.youtube.com/user/attilagyori/videos",
+    },
+  },
+};
