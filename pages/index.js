8f6a1aa090be608c30fbeff7308c5478fdd70bc4
Attila Gyori <attila.gyori.id@gmail.com>
2021-12-20 14:12:57 +0100

    head metadata

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
 /** @format */
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-export default function HomePage() {
+import { useEffect } from "react";
+import Head from "next/head";
+export default function HomePage({ title, keywords, description }) {
   const router = useRouter();
 
   useEffect(() => {
     router.push("/intro");
   }, []);
-  return <></>;
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name='keywords' content={keywords} />
+        <meta name='description' content={description} />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+    </>
+  );
 }
+
+HomePage.defaultProps = {
+  title: "Attila Győri | SDET & Developer, Designer and Photographer",
+  keywords:
+    "development, coding, programming, design, graphic, illustration, animation, qa, sdet, test automation",
+  description: "Attila Győri Developer personal site",
+};
