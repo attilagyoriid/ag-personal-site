21fc6fc67546a3be9b4939d34a46d82b5a337b68
Attila Gyori <attila.gyori.id@gmail.com>
2022-01-14 16:00:12 +0100

    image slide in

--- components/skills/skills.jsx
+++ components/skills/skills.jsx
@@ -1,10 +1,24 @@
 import Link from "next/link";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 import classes from "./skills.module.scss";
 
 import Skillbars from "../skillBars/skillbars";
 import TitleText from "../titleText/titleText";
 
 export default function Skills() {
+  const skillImgRef = useRef(null);
+  useEffect(() => {
+    gsap.from(skillImgRef.current, {
+      scrollTrigger: skillImgRef.current,
+      autoAlpha: 0,
+      x: -500,
+      duration: 2,
+      stagger: 0.25,
+      ease: "expo.easeInOut",
+      delay: 1,
+    });
+  }, []);
   return (
     <div className={classes["skills-container"]}>
       <TitleText
@@ -13,7 +27,7 @@ export default function Skills() {
         textColor="#f5f5f5"
       />
       <div className={classes["skills-container-details"]}>
-        <div className={classes["skill-image"]}>
+        <div className={classes["skill-image"]} ref={skillImgRef}>
           <img src="/images/strength.svg" alt="super power image" />
         </div>
         <div className={classes["skill-bar"]}>
