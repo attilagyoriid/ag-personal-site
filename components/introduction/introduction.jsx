63e9e4a8e78259d53c537c49f8ca03571cefb557
Attila Gyori <attila.gyori.id@gmail.com>
2022-01-14 11:50:42 +0100

    introduction component

---
+++ components/introduction/introduction.jsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import Hun from "../hun/hun";
+import TitleText from "../titleText/titleText";
+import classes from "./introduction.module.scss";
+function Introduction() {
+  return (
+    <div className={classes["introduction-container"]}>
+      <TitleText
+        bigText="hi, I am Attila"
+        bigTextBoldSubText="Attila"
+        smallText=""
+      />
+      <Hun />
+      <div className={classes["introduction-text"]}>
+        Hey, Thanks for stopping by. I am a software developer with a passion
+        for web design. I enjoy developing simple, clean and slick websites that
+        provide real value to the end user.
+      </div>
+      <div>
+        <Link href="/home#contact" passHref scroll={true}>
+          <a className={classes["btn-dark--outline"]}>Contact</a>
+        </Link>
+      </div>
+      <div className={classes["bottom-line"]}></div>
+    </div>
+  );
+}
+
+export default Introduction;
