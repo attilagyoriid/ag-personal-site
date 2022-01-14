fae07d6bea1eaf16ddb7ecd804fd81c81ace3f27
Attila Gyori <attila.gyori.id@gmail.com>
2022-01-14 11:45:43 +0100

    portrait img component

--- components/portraitScroll/portraitScroll.jsx
+++ components/portraitScroll/portraitScroll.jsx
@@ -1,27 +1,63 @@
 /** @format */
 import { useEffect, useState, useRef } from "react";
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import Image from "next/image";
 import classes from "./portraitScroll.module.scss";
 
 export default function PortraitScroll() {
   const portraitContainer = useRef(null);
   const imageScale = useRef(null);
+  const imageRef = useRef(null);
   useEffect(() => {
-    gsap.set(imageScale.current, {
-      xPercent: -50,
-      yPercent: -50,
-      autoAlpha: 0,
-    });
-    gsap.to(imageScale.current, {
-      scale: 10,
-      autoAlpha: 1,
-
-      scrollTrigger: {
-        trigger: portraitContainer.current,
-        start: "top 10%",
-        pin: portraitContainer.current,
-        scrub: true,
+    ScrollTrigger.matchMedia({
+      "(min-width: 1024px) and (orientation: landscape)": function () {
+        gsap.set(imageScale.current, {
+          autoAlpha: 0,
+          scale: 0,
+          y: 100,
+        });
+        gsap.to(imageScale.current, {
+          scale: 1,
+          autoAlpha: 1,
+          y: 0,
+          scrollTrigger: {
+            trigger: portraitContainer.current,
+            start: "top top",
+            end: "bottom bottom",
+            pinSpacing: true,
+            pin: true,
+            scrub: true,
+            stagger: 0.2,
+            toggleActions: "play none none none",
+            markers: true,
+            once: true,
+          },
+        });
+      },
+      "(max-width: 1024px)": function () {
+        gsap.set(imageScale.current, {
+          autoAlpha: 0,
+          scale: 0,
+          y: 100,
+        });
+        gsap.to(imageScale.current, {
+          scale: 1,
+          autoAlpha: 1,
+          y: 0,
+          scrollTrigger: {
+            trigger: portraitContainer.current,
+            start: "top 20%",
+            end: "bottom 20%",
+            pinSpacing: true,
+            pin: true,
+            scrub: true,
+            stagger: 0.2,
+            toggleActions: "play none none none",
+            markers: true,
+            once: true,
+          },
+        });
       },
     });
   }, []);
@@ -29,14 +65,7 @@ export default function PortraitScroll() {
   return (
     <div ref={portraitContainer} className={classes["portrait-container"]}>
       <div ref={imageScale} className={classes["img-scale"]}>
-        <Image
-          width={1920}
-          height={1080}
-          layout="responsive"
-          src={"/images/me-leader.png"}
-          alt="Attila Portrait"
-          objectFit="cover"
-        />
+        <img ref={imageRef} src={"/images/me.png"} alt="Attila Portrait" />
       </div>
     </div>
   );
