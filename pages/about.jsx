1b8f3421a7288f088b48957b265dcedd7daf3909
Attila Gyori <attila.gyori.id@gmail.com>
2022-01-14 11:50:25 +0100

    about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,11 +1,14 @@
 /** @format */
 
 import Footer from "../components/footer/footer";
+import FunFacts from "../components/funFacts/funFacts";
 import ImageLeadingWithTitle from "../components/imageLeadingWithTitle/imageLeadingWithTitle";
+import Introduction from "../components/introduction/introduction";
 import Layout from "../components/layout/layout";
+import MoreAbout from "../components/moreAbout/moreAbout";
 import PortraitScroll from "../components/portraitScroll/portraitScroll";
-import Skillbars from "../components/skillBars/skillbars";
-import Works from "../components/work/works";
+import Skills from "../components/skills/skills";
+import TitleText from "../components/titleText/titleText";
 
 export default function About() {
   return (
@@ -14,10 +17,20 @@ export default function About() {
       keywords="development, developer, software, software developer, software engineer, coding, programming, design, graphic, illustration, animation, qa, sdet, test automation, software test automation, software development engineer in test, Attila Győri, AG, Attila Gyori, Gyori Attila, Győri Attila, about, story"
       description="Attila Győri Sfotware Developer personal site | About"
     >
-      <ImageLeadingWithTitle url="/images/intro-whoami9.png" title="Who Am I" />
-      <Works />
+      <ImageLeadingWithTitle url="/images/intro-whoami" title="Who Am I" />
+      <Introduction />
       <PortraitScroll />
-      <Skillbars />
+
+      <TitleText
+        bigText="a few facts about me"
+        bigTextBoldSubText="facts"
+        smallText=""
+        textColor="#e9ce84"
+      />
+      <FunFacts />
+      <MoreAbout />
+
+      <Skills />
       <Footer />
     </Layout>
   );
