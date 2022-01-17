5260ca6622a9244255ae490754e8ab1fc3a9b26f
Attila Gyori <attila.gyori.id@gmail.com>
2022-01-17 08:42:16 +0100

    fade in animation

--- components/funFacts/funFacts.jsx
+++ components/funFacts/funFacts.jsx
@@ -1,6 +1,21 @@
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 import classes from "./funFacts.module.scss";
 
 export default function FunFacts() {
+  const funFactsImgRef = useRef(null);
+  useEffect(() => {
+    gsap.from(funFactsImgRef.current, {
+      scrollTrigger: funFactsImgRef.current,
+      autoAlpha: 0,
+
+      y: 200,
+      duration: 1.5,
+      stagger: 0.25,
+      ease: "expo.easeInOut",
+      delay: 0.5,
+    });
+  }, []);
   return (
     <section className={classes["facts_container"]}>
       <div className={classes["facts__img_container"]}>
@@ -18,7 +33,11 @@ export default function FunFacts() {
             <li></li>
           </ul>
         </div>
-        <img src="/images/fun-facts.svg" alt="Few facts about me" />
+        <img
+          src="/images/fun-facts-main.svg"
+          alt="Few facts about me"
+          ref={funFactsImgRef}
+        />
       </div>
     </section>
   );
