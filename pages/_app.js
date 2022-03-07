9895016ac1383696e2191950ae24b96f5ede45ac
Attila Gyori <attila.gyori.id@gmail.com>
2022-03-07 18:31:29 +0100

    Google Analytics wire up

--- pages/_app.js
+++ pages/_app.js
@@ -5,36 +5,69 @@ import { motion } from "framer-motion";
 import nProgress from "nprogress";
 import { Router, useRouter } from "next/router";
 import "nprogress/nprogress.css";
-
+import Script from "next/script";
+import * as gtag from "../services/gtag";
+import { useEffect } from "react";
 
 Router.events.on("routeChangeStart", nProgress.start);
 Router.events.on("routeChangeError", nProgress.done);
 Router.events.on("routeChangeComplete", nProgress.done);
 
 function MyApp({ Component, pageProps, router }) {
+  const routerX = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    routerX.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      routerX.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [routerX.events]);
   return (
-    <motion.div
-      key={router.route}
-      initial="initial"
-      animate="animate"
-      variants={{
-        initial: {
-          opacity: 0,
-          scale: 1,
-        },
-        animate: {
-          opacity: 1,
-          scale: 1,
-        },
-        exit: {
-          opacity: 0,
-          scale: 0.4,
-        },
-      }}
-      transition={{ duration: 0.9 }}
-    >
-      <Component {...pageProps} key={router.route} />
-    </motion.div>
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <motion.div
+        key={router.route}
+        initial="initial"
+        animate="animate"
+        variants={{
+          initial: {
+            opacity: 0,
+            scale: 1,
+          },
+          animate: {
+            opacity: 1,
+            scale: 1,
+          },
+          exit: {
+            opacity: 0,
+            scale: 0.4,
+          },
+        }}
+        transition={{ duration: 0.9 }}
+      >
+        <Component {...pageProps} key={router.route} />
+      </motion.div>
+    </>
   );
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,29 +1,12 @@
 /** @format */
 
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <Script
-            strategy="lazyOnload"
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <Script strategy="lazyOnload">
-            {`window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});`}
-          </Script>
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
