699ede79b8b33dd5070980ef8ff6144338e630a9
attilagyoriid <attila.gyori.id@gmail.com>
2022-05-30 08:44:51 +0200

    Specialization Card Contents

--- components/specialization/dataSpecDescription.js
+++ components/specialization/dataSpecDescription.js
@@ -13,18 +13,16 @@ const cardList = [
       autoPlay: false,
     },
     text: [
-      `1 Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-    Nostrum odio sapiente nobis, incidunt libero, blanditiis similique,
-    laudantium placeat eum quaerat hic id! Sed eligendi corporis consequatur, quia non nisi? Quisquam.`,
-      `2 Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-    Nostrum odio sapiente nobis, incidunt libero, blanditiis similique,
-    laudantium placeat eum quaerat hic id! Sed eligendi corporis consequatur, quia non nisi? Quisquam. Nostrum odio sapiente nobis, incidunt libero, blanditiis similique,`,
-      `3 Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-    Nostrum odio sapiente nobis, incidunt libero, blanditiis similique,
-    laudantium placeat eum quaerat hic id! Sed eligendi corporis consequatur, quia non nisi? Quisquam.
-    Nostrum odio sapiente nobis, incidunt libero, blanditiis similique,
-    laudantium placeat eum quaerat hic id! Sed eligendi corporis consequatur, quia non nisi? 3
-    laudantium placeat eum quaerat hic id! Sed eligendi corporis consequatur, quia non nisi? 3`,
+      `The Software Quality Assurance and espacially Software Test Automation are the fields I am the most expreinced in. 
+      I have been working in different positions
+      realted to Software Development Engineer in Test for almost 20 years now.`,`I love QA. 
+      The process, getting into the details. At the heart of it, 
+      it comes down to connection & communication between all the parts of a development team, 
+      from the project manager to developer to design and to the external clients at large. `,
+      `With over 19 years of experience in enterprise software product quality assurance, I possess the comprehensive skill set 
+      and hands-on understanding necessary to affect positive, overreaching outcomes in this role.
+      I am a self-motivated and progress-driven Quality focused SDET and Engineering Lead with an extensive background in multiple industries requiring 
+      comprehensive test automation (Desktop applications,APIs, Websites, Mobiles)`,
     ],
   },
   {
@@ -41,18 +39,18 @@ const cardList = [
       autoPlay: false,
     },
     text: [
-      `1 Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-    Nostrum odio sapiente nobis, incidunt libero, blanditiis similique,
-    laudantium placeat eum quaerat hic id! Sed eligendi corporis consequatur, quia non nisi? Quisquam.`,
-      `2 Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-    Nostrum odio sapiente nobis, incidunt libero, blanditiis similique,
-    laudantium placeat eum quaerat hic id! Sed eligendi corporis consequatur, quia non nisi? Quisquam. Nostrum odio sapiente nobis, incidunt libero, blanditiis similique,`,
-      `3 Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-    Nostrum odio sapiente nobis, incidunt libero, blanditiis similique,
-    laudantium placeat eum quaerat hic id! Sed eligendi corporis consequatur, quia non nisi? Quisquam.
-    Nostrum odio sapiente nobis, incidunt libero, blanditiis similique,
-    laudantium placeat eum quaerat hic id! Sed eligendi corporis consequatur, quia non nisi? 3
-    laudantium placeat eum quaerat hic id! Sed eligendi corporis consequatur, quia non nisi? 3`,
+      `I started coding before the age of ten.I have worked in a wide range of domains such as 3D modeling, Healthcare and Telecommunication.
+      I am a seasoned software expert who has developed products for enterprise clients such as General Electric, Ericsson and Graphisoft.
+      I specialize in building Test Automation Frameworks, and applications supporting Software Development and Test Automation / QA processes.`,
+      `I write code. A lot of code. I've been writing code over the years. In that time I've worked with and picked up many things. 
+      As a result, I'm often referred to by many as a polyglot. My main focus is back-end development with 10 years experience in java programming language,
+      and IOC container Spring Framework and Spring Boot. I am also experienced in and ethusiastic about CI using maven, Docker and Jenkins with particular
+      focus on code quality applying TDD using JUnit4 and JUnit5, TestNG, RestAssured, BDD Framework such as JBehave and Cucumber, API testing 
+      with Spring Cloud Contract and RestAssured, or Front-End test automation using Selenium or Cypress.
+      `,
+      `In the last 3 years I've tried my hand at front-end development. I have always found the visual customer facing part of software solutions to be the most compelling of
+      software product development. I got aquanted with javascript, typescript, html, csss and sass, and javascript frameworks such as ReactJS and NextJS in a self-taught way attending hundereds hours of online courses.
+      I is great addition for my everyday work being able to deliver not only APIs or CLI solutions but user aiding UIs as well`,
     ],
   },
   {
@@ -69,18 +67,17 @@ const cardList = [
       autoPlay: false,
     },
     text: [
-      `1 Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-    Nostrum odio sapiente nobis, incidunt libero, blanditiis similique,
-    laudantium placeat eum quaerat hic id! Sed eligendi corporis consequatur, quia non nisi? Quisquam.`,
-      `2 Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-    Nostrum odio sapiente nobis, incidunt libero, blanditiis similique,
-    laudantium placeat eum quaerat hic id! Sed eligendi corporis consequatur, quia non nisi? Quisquam. Nostrum odio sapiente nobis, incidunt libero, blanditiis similique,`,
-      `3 Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-    Nostrum odio sapiente nobis, incidunt libero, blanditiis similique,
-    laudantium placeat eum quaerat hic id! Sed eligendi corporis consequatur, quia non nisi? Quisquam.
-    Nostrum odio sapiente nobis, incidunt libero, blanditiis similique,
-    laudantium placeat eum quaerat hic id! Sed eligendi corporis consequatur, quia non nisi? 3
-    laudantium placeat eum quaerat hic id! Sed eligendi corporis consequatur, quia non nisi? 3`,
+      `I’ve always been someone who has both a creative and a logical side. Ever since I can remember, I was drawing characters and landscapes, painting. 
+       I have always felt especially strong affection towards photography and film-making.
+       I realized it would be the perfect fit. I could use my creative side to design and my logical side to code. Being able to not only code a full-stack software solution, 
+       but to be able to design the UI as well.`,
+      `I have took photos on a regular basis for 10 years now using Lightroom and Photoshop for retouch. After a while I started creating
+      seemless photo compositions creating unique fanatsy like photos. And I relized I could apply this knowledge as part of software development so 
+      I turned my to UI design, first using Photoshop, after that I got aquanted with Illustrator and Figma.
+      `,
+      `I am also a bit experienced in 3D modelling and digital sculpting using Autodesk Maya and Zbrush. On the top of all that, to boost the markting value of my work
+      I have started working on video editing and VFX using Adobe Premiere Pro and After Effects.
+      `,
     ],
   },
 ];
