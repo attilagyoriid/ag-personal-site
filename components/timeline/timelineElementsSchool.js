87607fe992dbd838a86bbaec9be8bae644c6f765
attilagyoriid <attila.gyori.id@gmail.com>
2022-05-30 16:04:53 +0200

    studies content

--- components/timeline/timelineElementsSchool.js
+++ components/timeline/timelineElementsSchool.js
@@ -1,61 +1,56 @@
 const timelineElements = [
+  
   {
     id: 1,
-    title: "Frontend Developer",
-    location: "Dragontail, Ascana",
+    title: "College of Modern Business Studies (EDUTUS)",
+    location: "Budapest, Hungary",
     description:
-      "Converting data to a graphical interface, through the use of HTML, CSS, and JavaScript, so that users can view and interact with that data.",
+    `BACHELOR DEGREE Major: 
+    Marketing Communication`,
 
-    date: "August 2016 - present",
-    icon: "work",
+    date: "2007 - 2009",
+    icon: "school",
   },
   {
     id: 2,
-    title: "Backend Developer",
-    location: "Skystead, Craonia",
+    title: "Eötvös Loránd University (ELTE)",
+    location: "Budapest, Hungary",
     description:
-      "Working hand-in-hand with front-end developers by providing the outward facing web application elements server-side logic. Creating the logic to make the web app function properly, and accomplishing this through the use of server-side scripting languages.",
+      `BACHELOR DEGREE Major: 
+       Computer Science`,
 
-    date: "June 2013 - August 2016",
-    icon: "work",
+    date: "1999 - 2003",
+    icon: "school",
   },
   {
     id: 3,
-    title: "Quality Assurance Engineer",
-    location: "South Warren, Geshington",
+    title: "Eötvös Loránd University (ELTE)",
+    location: "Budapest, Hungary",
     description:
-      "Assessing the quality of specifications and technical design documents in order to ensure timely, relevant and meaningful feedback.",
+      `BACHELOR DEGREE Major: 
+       Didatctics`,
 
-    date: "September 2011 - June 2013",
-    icon: "work",
+    date: "1999 - 2003",
+    icon: "school",
   },
   {
     id: 4,
-    title: "Oak Ridge College",
-    location: "South Warren, Geshington",
+    title: "Eötvös Loránd University (ELTE)",
+    location: "Budapest, Hungary",
     description:
-      "Online Course in Magical Beasts and Wonders of the World - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
+      `BACHELOR DEGREE Major:
+       English Language`,
 
-    date: "September 2011",
+    date: "1999 - 2003",
     icon: "school",
   },
   {
     id: 5,
-    title: "Hawking College",
-    location: "Skystead, Craonia",
+    title: "Neumann János Secondary School for Computer Technologies",
+    location: "Budapest, Hungary",
     description:
-      "College - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
-
-    date: "2007 - 2011",
-    icon: "school",
-  },
-  {
-    id: 6,
-    title: "Marble Hills Grammar School",
-    location: "Dragontail, Ascana",
-    description:
-      "Highschool - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
-    date: "2003 - 2007",
+      "Secondary school diploma in Computer Technologies",
+    date: "1994 - 1998",
     icon: "school",
   },
 ];
