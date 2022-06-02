2559f79e2d40946cda0583d522779323e7f580d4
attilagyoriid <attila.gyori.id@gmail.com>
2022-06-02 22:20:41 +0200

    Creative animation

--- components/creative/creative.jsx
+++ components/creative/creative.jsx
@@ -1,15 +1,43 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import Lottie from 'react-lottie';
+import designer_animation from "./designer_animation.json";
 import classes from "./creative.module.scss";
 export default function Creative() {
+  const leaderTextRef = useRef(null);
+  const leaderImgContainerRef = useRef(null);
+  const defaultOptions = {
+    loop: true,
+    autoplay: true, 
+    animationData: designer_animation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
 
   useEffect(() => {
-   
+    gsap.from(leaderTextRef.current, {
+      scrollTrigger: leaderTextRef.current,
+      autoAlpha: 0,
+      x: -200,
+      duration: 1.5,
+      ease: "expo.easeInOut",
+      delay: 1,
+    });
+    gsap.from(leaderImgContainerRef.current, {
+      scrollTrigger: leaderImgContainerRef.current,
+      autoAlpha: 0,
+      x: 200,
+      duration: 1.5,
+      ease: "expo.easeInOut",
+      delay: 1,
+    });
   }, []);
   return (
     <div className={classes["leader__container"]}>
       
       
-      <div className={classes["leader_text"]} >
+      <div className={classes["leader_text"]} ref={leaderTextRef}>
         <div className={classes["leader_text__container"]}>
           <div className={classes["leader_text"]}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
@@ -19,11 +47,13 @@ export default function Creative() {
           </div>
         </div>
       </div>
-      <div className={classes["leader_text__container"]} >
+      <div className={classes["leader_img__container"]} ref={leaderImgContainerRef}>
       
         <div className={classes["leader_img--lead"]}>being a creative...</div>
         <div className={classes["leader_img"]}>
-          <img src="/images/illustration-designer_left.png" alt="Creative illustration" />
+          <Lottie options={defaultOptions}
+                height={700}
+                width={700} />
         </div>
       </div>
     </div>
