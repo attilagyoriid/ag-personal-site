0f2c2f5b2b3237004cb5d31fb60256a2a74f5024
attilagyoriid <attila.gyori.id@gmail.com>
2022-06-02 22:20:24 +0200

    Development animation

--- components/developer/developer.jsx
+++ components/developer/developer.jsx
@@ -1,13 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import Lottie from 'react-lottie';
+import developer_animation from "./developer_animation.json";
 import classes from "./developer.module.scss";
 export default function Developer() {
+  const devTextRef = useRef(null);
+  const devImgContainerRef = useRef(null);
+  const defaultOptions = {
+    loop: true,
+    autoplay: true, 
+    animationData: developer_animation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
 
   useEffect(() => {
-   
+    gsap.from(devTextRef.current, {
+      scrollTrigger: devTextRef.current,
+      autoAlpha: 0,
+      x: -200,
+      duration: 1.5,
+      ease: "expo.easeInOut",
+      delay: 1,
+    });
+    gsap.from(devImgContainerRef.current, {
+      scrollTrigger: devImgContainerRef.current,
+      autoAlpha: 0,
+      x: 200,
+      duration: 1.5,
+      ease: "expo.easeInOut",
+      delay: 1,
+    });
   }, []);
   return (
     <div className={classes["dev__container"]}>
-      <div className={classes["dev_text"]} >
+      <div className={classes["dev_text"]} ref={devTextRef}>
         <div className={classes["dev_text__container"]}>
           <div className={classes["dev_text"]}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
@@ -17,11 +45,13 @@ export default function Developer() {
           </div>
         </div>
       </div>
-      <div className={classes["dev_text__container"]} >
+      <div className={classes["dev_img__container"]} ref={devImgContainerRef}>
       
         <div className={classes["dev_img--lead"]}>being a developer...</div>
         <div className={classes["dev_img"]}>
-          <img src="/images/illustration-dev_left.png" alt="Developer illustration" />
+        <Lottie options={defaultOptions}
+              height={500}
+              width={500} />
         </div>
       </div>
       
