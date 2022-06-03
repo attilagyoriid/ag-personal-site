d304adef253f260fd82bc44c13128ae7106da081
attilagyoriid <attila.gyori.id@gmail.com>
2022-06-03 14:35:58 +0200

    text content

--- components/leader/leader.jsx
+++ components/leader/leader.jsx
@@ -48,10 +48,29 @@ export default function Leader() {
       <div className={classes["leader_text"]} >
         <div className={classes["leader_text__container"]} ref={leaderTextContainerRef}>
           <div className={classes["leader_text"]}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-            impedit, ipsum maxime ex alias libero suscipit? Quibusdam tenetur,
-            repudiandae ex facere pariatur molestiae debitis fugit, aperiam
-            voluptas alias dolores saepe.
+            I have been a development team lead for more them 5 years now.
+            I do not reallyy want to appear to show off, 
+            but I have always been the one to set the tone, and others have liked to cosider me as a leader in some way since I can remember 
+            (from the primary school on). I am kind of a "natural born leader" 
+
+            Currently I am a Team Lead to a team of Teast Automation Framework engineers, establishing code standards, performing code reviews, coding, 
+            mentoring, giving recommendations on architectural issues, facilitating scrum sceremonies, and doing behind-the-scenes operational work,
+                        
+            Leaing development efforts in agile/scrum environment with CI/CD automation mindset and DevOps culture, 
+            organizing and distributing tasks and workloads considering and planning with the current resources. 
+            As part of the Leader position I am attending technical interviews helping to recruit many of the engineers on the team, organizing trainings,
+            helpint to form and achieve career goals and carry out and follow-up performance reviews. 
+            My main objective as a Leader is to make and keep every member of
+            my team motivated and insipred profesinally and personally providing the perception of being represented in the organization, being valuable assets with
+            having a clear goal, a sense of progression and achievement and purpose in a production that really matters.
+            
+            For me, people come first, without a solid foundation and trust it is impossible to create something valuable.
+            Helping others to grow and achieve their goals is one of the most rewarding activities in life.
+            I also studied psychology, pedagogy and didactics at college for 4 years wihich really helps me in working with individuals and teams 
+            (of course this fact doesnt make me a psychologist though &#128516;)
+            So I consider myself an involving leader to whom your opinion really matters, a so called "servant leader".
+            Due to this aspect of my attitude and my great network in the industry I could easly ramp-up a team of 5-7 great professionals whitin a few month,
+            professionals I worked with in mutual trust and respect and sharing the willingness to work together again in the future.
           </div>
         </div>
       </div>
