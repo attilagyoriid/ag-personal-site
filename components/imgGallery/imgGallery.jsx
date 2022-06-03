66dde84b314725cf0001c854e6e49804b3ff438e
attilagyoriid <attila.gyori.id@gmail.com>
2022-06-03 17:09:30 +0200

    Gallery component hook up

--- components/imgGallery/imgGallery.jsx
+++ components/imgGallery/imgGallery.jsx
@@ -4,40 +4,49 @@ import "react-image-gallery/styles/css/image-gallery.css";
 
 const images = [
   {
-    original: "https://picsum.photos/id/1018/1000/600/",
-    thumbnail: "https://picsum.photos/id/1018/250/150/",
+    original: "/images/gallery/my_parents_opt.jpg",
+    thumbnail: "/images/gallery/my_parents_opt.jpg",
+    description: "My beloved parents and the baby me",
   },
   {
-    original: "https://picsum.photos/id/1015/1000/600/",
-    thumbnail: "https://picsum.photos/id/1015/250/150/",
+    original: "/images/gallery/new_sherrif_in_the_town_opt.jpg",
+    thumbnail: "/images/gallery/new_sherrif_in_the_town_opt.jpg",
+    description: "new sherrif in the town",
   },
   {
-    original: "https://picsum.photos/id/1019/1000/600/",
-    thumbnail: "https://picsum.photos/id/1019/250/150/",
+    original: "/images/gallery/natural_born_photographer_opt.jpg",
+    thumbnail: "/images/gallery/natural_born_photographer_opt.jpg",
+    description: "natural born photographer",
   },
   {
-    original: "https://picsum.photos/id/1018/1000/600/",
-    thumbnail: "https://picsum.photos/id/1018/250/150/",
+    original: "/images/gallery/hungarian_jakuzzi_opt.jpg",
+    thumbnail: "/images/gallery/hungarian_jakuzzi_opt.jpg",
+    description: "hungarian jacuzzi",
   },
   {
-    original: "https://picsum.photos/id/1015/1000/600/",
-    thumbnail: "https://picsum.photos/id/1015/250/150/",
+    original: "/images/gallery/ovoda1_opt.jpg",
+    thumbnail: "/images/gallery/ovoda1_opt.jpg",
+    description: "in kindergarten",
   },
   {
-    original: "https://picsum.photos/id/1019/1000/600/",
-    thumbnail: "https://picsum.photos/id/1019/250/150/",
+    original: "/images/gallery/karate_kid_opt.jpg",
+    thumbnail: "/images/gallery/karate_kid_opt.jpg",
+    description: "karate kid",
   },
   {
-    original: "https://picsum.photos/id/1018/1000/600/",
-    thumbnail: "https://picsum.photos/id/1018/250/150/",
+    original: "/images/gallery/graduation_opt.jpg",
+    thumbnail: "/images/gallery/graduation_opt.jpg",
+    description: "elementary school graduation",
   },
   {
-    original: "https://picsum.photos/id/1015/1000/600/",
-    thumbnail: "https://picsum.photos/id/1015/250/150/",
+    original: "/images/gallery/high_school_class_photo_opt.jpg",
+    thumbnail: "/images/gallery/high_school_class_photo_opt.jpg",
+    description: "hightschool school",
   },
-  {
-    original: "https://picsum.photos/id/1019/1000/600/",
-    thumbnail: "https://picsum.photos/id/1019/250/150/",
+  {   
+    original: "/images/gallery/badboyz_opt.jpg",
+    thumbnail: "/images/gallery/badboyz_opt.jpg",
+    description: "bad boyz",
   },
 ];
 
