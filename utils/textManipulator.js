acdfa5e24fedd48a465f24aff3fafeaf2b995bd9
attilagyoriid <attila.gyori.id@gmail.com>
2022-06-19 12:29:00 +0200

    separating text section with a whole word at the end of the section

--- utils/textManipulator.js
+++ utils/textManipulator.js
@@ -1,10 +1,31 @@
 export function chunkSubstrBySegments(str, segments) {
-    const numChunks = Math.ceil(str.length / segments)
-    const chunks = new Array(segments)
-  
-    for (let i = 0, o = 0; i < segments; i++, o += numChunks) {
-      chunks[i] = str.substr(o, numChunks)
+  let sectionCharLenght = Math.ceil(str.length / segments)
+  const chunks = new Array(segments)
+  let j = 0;
+  let h = 0; 
+  console.log("string lenght",str.length);
+  for (let i = 0, o = 0; i < segments; i++, o += sectionCharLenght) {
+
+    chunks[i] = str.substr(o-j, sectionCharLenght-j);
+
+    chunks[i] = chunks[i].slice(h); 
+   
+    console.log("current chunk",chunks[i]);
+    console.log("end of the chunk",o);
+    console.log("end of the chunk minus the plus characters",sectionCharLenght-j);
+    
+    if (str[sectionCharLenght] && str[sectionCharLenght] !== ' ' ) {
+      j = sectionCharLenght;
+      while (str[j] !== ' ') {
+        chunks[i] += str[j];
+        j++;
+        h++;
+      }
+      sectionCharLenght += j;
+    } else {
+      h = 0;
     }
-  
-    return chunks
   }
+
+  return chunks
+}
