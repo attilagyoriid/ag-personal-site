63cdf1a57934f2fc63aa725fe7126dae986c81c0
attilagyoriid <attila.gyori.id@gmail.com>
2022-06-20 20:41:17 +0200

    console log deleted

--- utils/textManipulator.js
+++ utils/textManipulator.js
@@ -3,16 +3,10 @@ export function chunkSubstrBySegments(str, segments) {
   const chunks = new Array(segments)
   let j = 0;
   let h = 0; 
-  console.log("string lenght",str.length);
   for (let i = 0, o = 0; i < segments; i++, o += sectionCharLenght) {
 
     chunks[i] = str.substr(o-j, sectionCharLenght-j);
-
     chunks[i] = chunks[i].slice(h); 
-   
-    console.log("current chunk",chunks[i]);
-    console.log("end of the chunk",o);
-    console.log("end of the chunk minus the plus characters",sectionCharLenght-j);
     
     if (str[sectionCharLenght] && str[sectionCharLenght] !== ' ' ) {
       j = sectionCharLenght;
