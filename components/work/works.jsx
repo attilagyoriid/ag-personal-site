5c8be18f98101fac58a548ab7e03b8478b5ab523
attilagyoriid <attila.gyori.id@gmail.com>
2022-06-20 16:58:04 +0200

    proper links for projects

--- components/work/works.jsx
+++ components/work/works.jsx
@@ -21,115 +21,133 @@ export default function Works() {
       />
       <div className={classes["container"]}>
         <div className={classes["items"]} onClick={handleClick}>
-          <div className={classes["item"]}>
-            <div className={classes["item-image"]}>
-              <img src="/images/works/item1.png" alt="" />
-            </div>
-            <div className={classes["item-text"]}>
-              <div className={classes["item-text-wrap"]}>
-                <p className={classes["item-text-category"]}>Design</p>
-                <h2 className={classes["item-text-title"]}>Great Gradients</h2>
+          <a target="_blank" href="/404" rel="noopener noreferrer">
+            <div className={classes["item"]}>
+              <div className={classes["item-image"]}>
+                <img src="/images/works/item1.png" alt="" />
+              </div>
+              <div className={classes["item-text"]}>
+                <div className={classes["item-text-wrap"]}>
+                  <p className={classes["item-text-category"]}>Design</p>
+                  <h2 className={classes["item-text-title"]}>Company Logo</h2>
+                </div>
               </div>
             </div>
-          </div>
-          <div className={classes["item"]}>
-            <div className={classes["item-image"]}>
-              <img src="/images/works/javascript_algo.jpg" alt="" />
-            </div>
-            <div className={classes["item-text"]}>
-              <div className={classes["item-text-wrap"]}>
-                <p className={classes["item-text-category"]}>Javascript</p>
-                <h2 className={classes["item-text-title"]}>Algorithms</h2>
+          </a>
+          <a target="_blank" href="https://github.com/attilagyoriid/algorithms-js" rel="noopener noreferrer">
+            <div className={classes["item"]}>
+              <div className={classes["item-image"]}>
+                <img src="/images/works/javascript_algo.jpg" alt="" />
+              </div>
+              <div className={classes["item-text"]}>
+                <div className={classes["item-text-wrap"]}>
+                  <p className={classes["item-text-category"]}>Javascript</p>
+                  <h2 className={classes["item-text-title"]}>Algorithms</h2>
+                </div>
               </div>
             </div>
-          </div>
-          <div className={classes["item"]}>
-            <div className={classes["item-image"]}>
-              <img src="/images/works/item3.png" alt="" />
-            </div>
-            <div className={classes["item-text"]}>
-              <div className={classes["item-text-wrap"]}>
-                <p className={classes["item-text-category"]}>
-                  Photography & Design
-                </p>
-                <h2 className={classes["item-text-title"]}>Vanishing</h2>
+          </a>
+          <a target="_blank" href="https://www.behance.net/gallery/141240137/Puppy-in-fantasy-forest" rel="noopener noreferrer">
+            <div className={classes["item"]}>
+              <div className={classes["item-image"]}>
+                <img src="/images/works/item3.png" alt="" />
+              </div>
+              <div className={classes["item-text"]}>
+                <div className={classes["item-text-wrap"]}>
+                  <p className={classes["item-text-category"]}>
+                    Photography & Design
+                  </p>
+                  <h2 className={classes["item-text-title"]}>Fantasy</h2>
+                </div>
               </div>
             </div>
-          </div>
-          <div className={classes["item"]}>
-            <div className={classes["item-image"]}>
-              <img src="/images/works/item4.png" alt="" />
-            </div>
-            <div className={classes["item-text"]}>
-              <div className={classes["item-text-wrap"]}>
-                <p className={classes["item-text-category"]}>Design</p>
-                <h2 className={classes["item-text-title"]}>Gap INC</h2>
+          </a>
+          <a target="_blank" href="https://github.com/attilagyoriid/leetcode_python" rel="noopener noreferrer">
+            <div className={classes["item"]}>
+              <div className={classes["item-image"]}>
+                <img src="/images/works/item4.png" alt="" />
+              </div>
+              <div className={classes["item-text"]}>
+                <div className={classes["item-text-wrap"]}>
+                  <p className={classes["item-text-category"]}>Python</p>
+                  <h2 className={classes["item-text-title"]}>Algorithms</h2>
+                </div>
               </div>
             </div>
-          </div>
-          <div className={classes["item"]}>
-            <div className={classes["item-image"]}>
-              <img src="/images/works/item5.png" alt="" />
-            </div>
-            <div className={classes["item-text"]}>
-              <div className={classes["item-text-wrap"]}>
-                <p className={classes["item-text-category"]}>
-                  Mobile UI Design
-                </p>
-                <h2 className={classes["item-text-title"]}>
-                  Face THe Experience
-                </h2>
+          </a>
+          <a target="_blank" href="/404" rel="noopener noreferrer">
+            <div className={classes["item"]}>
+              <div className={classes["item-image"]}>
+                <img src="/images/works/item5.png" alt="" />
+              </div>
+              <div className={classes["item-text"]}>
+                <div className={classes["item-text-wrap"]}>
+                  <p className={classes["item-text-category"]}>
+                    Mobile UI Design
+                  </p>
+                  <h2 className={classes["item-text-title"]}>
+                    Linda Novai
+                  </h2>
+                </div>
               </div>
             </div>
-          </div>
-          <div className={classes["item"]}>
-            <div className={classes["item-image"]}>
-              <img src="/images/works/item6.png" alt="" />
-            </div>
-            <div className={classes["item-text"]}>
-              <div className={classes["item-text-wrap"]}>
-                <p className={classes["item-text-category"]}>Design Concept</p>
-                <h2 className={classes["item-text-title"]}>Bezier Curves</h2>
+          </a>
+          <a target="_blank" href="https://dribbble.com/shots/17981216-Desktop-Landing-Page-Design-for-a-coach" rel="noopener noreferrer">
+            <div className={classes["item"]}>
+              <div className={classes["item-image"]}>
+                <img src="/images/works/item6.png" alt="" />
+              </div>
+              <div className={classes["item-text"]}>
+                <div className={classes["item-text-wrap"]}>
+                  <p className={classes["item-text-category"]}>Design Concept</p>
+                  <h2 className={classes["item-text-title"]}>Personal Coach</h2>
+                </div>
               </div>
             </div>
-          </div>
-          <div className={classes["item"]}>
-            <div className={classes["item-image"]}>
-              <img src="/images/works/java_development.jpg" alt="" />
-            </div>
-            <div className={classes["item-text"]}>
-              <div className={classes["item-text-wrap"]}>
-                <p className={classes["item-text-category"]}>Java</p>
-                <h2 className={classes["item-text-title"]}>Dock Ponder</h2>
+          </a>
+          <a target="_blank" href="https://github.com/attilagyoriid/leetcode_java" rel="noopener noreferrer">
+            <div className={classes["item"]}>
+              <div className={classes["item-image"]}>
+                <img src="/images/works/java_development.jpg" alt="" />
+              </div>
+              <div className={classes["item-text"]}>
+                <div className={classes["item-text-wrap"]}>
+                  <p className={classes["item-text-category"]}>Java</p>
+                  <h2 className={classes["item-text-title"]}>Algorithms</h2>
+                </div>
               </div>
             </div>
-          </div>
-          <div className={classes["item"]}>
-            <div className={classes["item-image"]}>
-              <img src="/images/works/item8.png" alt="" />
-            </div>
-            <div className={classes["item-text"]}>
-              <div className={classes["item-text-wrap"]}>
-                <p className={classes["item-text-category"]}>
-                  Web Applications
-                </p>
-                <h2 className={classes["item-text-title"]}>Restaurant App</h2>
+          </a>
+          <a target="_blank" href="https://github.com/attilagyoriid/ag-personal-site" rel="noopener noreferrer">
+            <div className={classes["item"]}>
+              <div className={classes["item-image"]}>
+                <img src="/images/works/item8.png" alt="" />
+              </div>
+              <div className={classes["item-text"]}>
+                <div className={classes["item-text-wrap"]}>
+                  <p className={classes["item-text-category"]}>
+                    Web Applications
+                  </p>
+                  <h2 className={classes["item-text-title"]}>Portfolio Site</h2>
+                </div>
               </div>
             </div>
-          </div>
-          <div className={classes["item"]}>
-            <div className={classes["item-image"]}>
-              <img src="/images/works/selenium.jpg" alt="" />
-            </div>
-            <div className={classes["item-text"]}>
-              <div className={classes["item-text-wrap"]}>
-                <p className={classes["item-text-category"]}>
-                  Social Network Concept
-                </p>
-                <h2 className={classes["item-text-title"]}>FriendFeed</h2>
+          </a>
+          <a target="_blank" href="/404" rel="noopener noreferrer">
+            <div className={classes["item"]}>
+              <div className={classes["item-image"]}>
+                <img src="/images/works/selenium.jpg" alt="" />
+              </div>
+              <div className={classes["item-text"]}>
+                <div className={classes["item-text-wrap"]}>
+                  <p className={classes["item-text-category"]}>
+                    Selenium
+                  </p>
+                  <h2 className={classes["item-text-title"]}>UI Automation</h2>
+                </div>
               </div>
             </div>
-          </div>
+          </a>
         </div>
       </div>
       <div className={classes["button-container"]}>
@@ -146,6 +164,6 @@ export default function Works() {
           </Link>
         </div>
       </div>
-    </section>
+    </section >
   );
 }
