412e77ac7401dad71f98ce9172aedf87ef3a96d6
attilagyoriid <attila.gyori.id@gmail.com>
2022-06-28 11:51:16 +0200

    ios platform check

---
+++ utils/platform.js
@@ -0,0 +1,15 @@
+export const isIOS = () => {
+    if (/iPad|iPhone|iPod/.test(navigator.platform)) {
+        return true;
+    } else {
+        return navigator.maxTouchPoints &&
+            navigator.maxTouchPoints > 2 &&
+            /MacIntel/.test(navigator.platform);
+    }
+}
+
+export const isIpadOS = () => {
+    return navigator.maxTouchPoints &&
+        navigator.maxTouchPoints > 2 &&
+        /MacIntel/.test(navigator.platform);
+}
