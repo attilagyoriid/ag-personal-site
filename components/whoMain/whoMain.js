4e3facd022220709e9df614ca78fc741a86b2af5
attilagyoriid <attila.gyori.id@gmail.com>
2022-09-11 13:56:39 +0200

    change download button

--- components/whoMain/whoMain.js
+++ components/whoMain/whoMain.js
@@ -3,9 +3,10 @@ import TitleSection from "../titleSection/titleSection";
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import classes from "./whoMain.module.scss";
-import DownloadButton from "../downloadButton/downloadButton";
 import WordCloud from "../wordCloud/wordCloud";
 import Link from "next/link";
+import * as gtag from "../../services/gtag";
+
 
 export default function WhoMain() {
   const bkgImgRef = useRef(null);
@@ -42,6 +43,14 @@ export default function WhoMain() {
       delay: 1,
     });
   }, []);
+  
+  const handleClick = (e) => {
+    gtag.event({
+      action: "download_cv",
+      category: "Download",
+      label: "download_cv",
+    });
+  };
   return (
     <section className={classes["main-who"]}>
       <img
@@ -58,22 +67,31 @@ export default function WhoMain() {
       <div className={classes["about-section"]}>
         <div ref={sphereSectionRef} className={classes["skill-sphere-section"]}>
           <WordCloud />
-          <DownloadButton />
+          <div className={`${classes["btn-dark--outline"]} ${classes["flex-center"]}`}>
+            <a
+              onClick={handleClick}
+              href="/doc/Attila_Gyori_2022.pdf"
+              target="_blank"
+              className={classes.dnbtn}
+            >Resume</a>
+          </div>
+
         </div>
 
         <div ref={textSectionRef} className={classes["text-section"]}>
           <p>
-          Not a Yet Another Average Software Engineer.
-          I'm passionate about bringing both the technical and visual aspects of digital products to life.
-          I’ve always been someone who has both a creative and a logical side.
-          I realized it would be the perfect fit. I could use my creative side to design and my logical side to code.    
-          I am a Software Test Automation Framework Developer and a Full-stack Engineer with over 19 years of Information Technology related experience. 
-          Sometimes I am also get involded in activites such as digital multimedia & ui design, graphic craftsmanship, and team management. 
-          I maintain a passion for technology and helping others get the most out of technology by using the appropriate tools, techniques, and strategies to ensure clients satisfaction.
-          Regarding the last 5 years I am functioning as a Development Team Lead as well.
+            Not a Yet Another Average Software Engineer.
+            I'm passionate about bringing both the technical and visual aspects of digital products to life.
+            I’ve always been someone who has both a creative and a logical side.
+            I realized it would be the perfect fit. I could use my creative side to design and my logical side to code.
+            I am a Software Test Automation Framework Developer and a Full-stack Engineer with over 19 years of Information Technology related experience.
+            Sometimes I am also get involded in activites such as digital multimedia & ui design, graphic craftsmanship, and team management.
+            I maintain a passion for technology and helping others get the most out of technology by using the appropriate tools, techniques, and strategies to ensure clients satisfaction.
+            Regarding the last 5 years I am functioning as a Development Team Lead as well.
           </p>
           <Link href="/about">
-            <a className={classes["btn-main"]}>About</a>
+
+            <a className={`${classes["btn-main"]} ${classes["btn-pulse"]}`}>About</a>
           </Link>
         </div>
       </div>
