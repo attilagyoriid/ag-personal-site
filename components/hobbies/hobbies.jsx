c8e589d3bfd36e5c38f4a9aa713814e879e0e0c7
attilagyoriid <attila.gyori.id@gmail.com>
2022-09-17 09:44:29 +0200

    description change

--- components/hobbies/hobbies.jsx
+++ components/hobbies/hobbies.jsx
@@ -23,24 +23,20 @@ export default function Hobbies() {
         <div className={classes["text-section"]}>
           <p>
             I am passionate about leveraging my diverse backgrounds to decipher challenging problems and create delightful experiences.
-            I do beleive in work-life balance. You cant succeed if you concentrate on one aspect of life only. 
-            Since private life is the main mitovation driver for excell at work, it could be also a source of inspiration for work, 
-            and vica-versa.
-            I think it’s important to get regular screen downtime, so when I’m not pushing pixels or coding, 
-            you’ll find me working on my film ideas, 3d model and screewriting, tending to my garden and many house plants, 
-            cooking up one of my favourite dishes, or hanging out with friends and family.
-            I am reading a lot, it is a seance of my bed time to read some pulp fiction,or books on topics like sociology or psychology.
-            I've also trying my hand at playing the guitar time and time again... Lets say I am not a natural born talent &#128516; and I love singing too.
-            I have a serious passion for movies so I watch lots of films and series - I am a real movie junkie.
+            I also believe a healthy work-life balance is important. You can't succeed if you concentrate on one aspect of life only. 
+            Since achieving a quality private life is the main motivational factor in professional excellence, it may also be a source of inspiration for work, and vice-versa.
+            I believe it is important to get regular downtime away from screens, so when I am not pushing pixels or coding, you can find me working on my film ideas, 3d modeling, 
+            screenwriting, tending to my garden and many houseplant, cooking one of my favorite dishes, or hanging out with friends and family.
+            I am reading a lot, and it is part of my regular bedtime routine to read some pulp fiction or books on topics like sociology or psychology.
+            I also have been trying my hand at playing guitar, but let's just say I am not a natural-born talent &#128516; I love singing as well.
+            I have a serious passion for movies and television, so I am frequently binge-watching.
             
-            I love travelling and taking photos, this is the mere essence of being for me getting acquanted with different cultures and people, making new friendships, 
-            sightseeing beautiful architecures and the miracle of nature. I also regularly do some sport, soccer, table tennis, biking or just stop by the gym. 
-            I love pets and I have a dog, a Havanese Bichion called "Whopper".
-            And of course I use my free time to learn or try new things.
-            My abundant energy fuels me in the pursuit of many interests, hobbies, areas of study and artistic endeavors. 
-            I’m a fast learner, able to pick up new skills and juggle different projects and roles with relative ease.
+            I love to travel and to take photos, this is the mere essence of being for me becoming acquanted with different cultures and people, making new friendships, 
+            and taking in beautiful architecture and the miracle of nature.  I also regularly engage in sports like soccer, table tennis, biking or just stopping by the gym.
+            I love animals, particularly my Havanese Bichon dog named Whopper.
+            Of course, I also like to use my spare time to learn or try new things.  My abundant energy fuels me in the pursuit of many interests, hobbies, areas of study, and artistic endeavors.    
+            I am a fast learner, and am able to pick up new skills, and juggle different projects and roles with relative ease. 
           </p>
-
         </div>
       </div>
     </section>
