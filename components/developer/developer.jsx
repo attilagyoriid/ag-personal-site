24f965ccdd1fd49bab43d06165148a5cdf9ae3d6
attilagyoriid <attila.gyori.id@gmail.com>
2022-09-17 09:44:11 +0200

    description change

--- components/developer/developer.jsx
+++ components/developer/developer.jsx
@@ -7,23 +7,23 @@ import TextCarousel from "../text-carousel/textCarousel";
 import { chunkSubstrBySegments } from "../../utils/textManipulator";
 export default function Developer() {
   const developer_animation = JSON.parse(JSON.stringify(developer_animation_data));
-  const text = `I love sofware development or any creative activity: Creating Something out of Nothing!
-  I have been working with technology for as long as I can remember.
+  const text = `I love sofware development or and creativity: Creating Something out of Nothing!
+  I have been working with technology as far as i can remember.
   I am eager to embrace new ideas, methodologies and technologies to expand my knowledge.
-  I maintain a passion for technology and helping others get the 
+  I maintain a passion for technology and for helping others get the 
   most out of technology by using the appropriate tools, techniques, and strategies to ensure clients satisfaction.
-  I have a passion for constantly challenging myself and learning new skills that I can apply to both work and everyday life. 
-  I have been involved in developing and testing backend services, apis, web and desktop applications, or implementing test automation frameworks helping other Engineers
-  to test their features delivered
-  My main experience is in Back-end development and Software QA, espacially in Software Quality Life Cycle, process formation & improvements and most of all Test Automation at all levels. But Regarding the last 3 years
-  I have gotten aquanted with front-end technolgies as well, such as HTML, CSS, Sass, Javascript, Typescript, ReactJs and NextJs. 
-  I enjoy getting to know the business, its goals, postions with roles and respos and the SDLC / STLC processes and workflows at the current company / customer, in the possesion of those information I can find the proper way 
-  how I can best apply myself to the business / company I am working for.
+  I enjoy constantly challenging myself and learning new skills that may be applied to both work and everyday life.
+  I have been involved in developing and testing backend services, apis, web / desktop applications, along with implementing test automation frameworks helping other Engineers
+  to test their features delivered.
+  My primary professional experience is in back-end development and software QA, especially Software Quality Life Cycle, processing formations and improvements, and particularly Test Automation at all levels. Over the past 3 years, 
+  I have become more familiar and comfortable with front-end technologies, like HTML, CSS, Sass, Javascript, Typescript, ReactJs and NextJs. 
+  I truly enjoy learning the client's business, its goals, the manner of its positions, roles, and SDLC/STLC processes and workflows at the current company / customer. 
+  Through that understanding, I am able to determine the proper manner in which I may best apply my skills to fit the client's needs. 
   I am a big advocate of reusable and maintainable easy to read and understand code, and I beleive in the applicability of clean code.
-  Implementing a function is not enought, it must be maintainable, extensible, self-documenting and future-proof. So the software QA and automated regression test set is not 
-  something to forget about in the face of time or budge constraints, it would like building a house without proper foundation. In the 21th century in IT industry there are no such attitudes like "it is just test",
-  Nooo, it is the organic part of the implemented functionality. Without proper safe-guards, nothing guarantees that the code will withstand the test of time! At the end of the day the customer satisfaction what really matters,
-  and without rock-solid code base, you wont be able to react the customers needs delivering new and new releases that they are willing to pay for.`
+  Implementing mere functionality is not sufficient, it must be maintainable, extensible, self-documenting and future-proof. As such, the software QA process and automation regression test set is not to be ignored in light of time or budget constraints. 
+  To do so would be akin to building a house without the proper foundation. In the 21th century in IT industry, software QA cannot be merely viewed as "just a test", 
+  Rather, it is an integral part of  the intended, implemented functionality. Only with the proper safe-guards in place, will quality code stand the test of time! At the end of the day the customer satisfaction what really matters,
+  and without rock-solid code base, you wont be able to react customers needs, delivering new and new releases they are willing to pay for.`
   const textContent = chunkSubstrBySegments(text,2);
   const devTextRef = useRef(null);
   const devImgContainerRef = useRef(null);
